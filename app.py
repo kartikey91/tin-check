bddcf3df22d924ed6094dc5135e8ebd56cda1397
Ubuntu <ubuntu@ip-172-31-58-246.ec2.internal>
2016-12-05 21:26:00 +0000

    deployment changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, render_template
 from tin_app import *
+from flask.ext.cors import CORS
 import sys
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def home():
@@ -30,4 +31,4 @@ def check_tin(tin):
 
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', debug=True, port=5000)
